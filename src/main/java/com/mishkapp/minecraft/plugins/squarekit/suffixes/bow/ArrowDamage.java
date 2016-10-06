ae83c186c8dfa300ef98ecbdb4b5997eebfb84da
mishkapp <mishkapp@gmail.com>
2016-10-06 22:24:05 +0700

    Added message for ArrowDamage and removed debug

--- src/main/java/com/mishkapp/minecraft/plugins/squarekit/suffixes/bow/ArrowDamage.java
+++ src/main/java/com/mishkapp/minecraft/plugins/squarekit/suffixes/bow/ArrowDamage.java
@@ -1,11 +1,12 @@
 package com.mishkapp.minecraft.plugins.squarekit.suffixes.bow;
 
+import com.mishkapp.minecraft.plugins.squarekit.Formatters;
 import com.mishkapp.minecraft.plugins.squarekit.KitPlayer;
+import com.mishkapp.minecraft.plugins.squarekit.Messages;
 import com.mishkapp.minecraft.plugins.squarekit.events.ArrowHitEntityEvent;
 import com.mishkapp.minecraft.plugins.squarekit.events.KitEvent;
 import com.mishkapp.minecraft.plugins.squarekit.suffixes.Suffix;
 import org.spongepowered.api.item.inventory.ItemStack;
-import org.spongepowered.api.text.format.TextColors;
 
 /**
  * Created by mishkapp on 06.10.2016.
@@ -28,15 +29,14 @@ public class ArrowDamage extends Suffix {
             if(!isWeaponInHand()){
                 return;
             }
-            System.out.println("arrow = " + ((ArrowHitEntityEvent) event).getArrow());
-            ArrowHitEntityEvent arrowHitEntityEvent = (ArrowHitEntityEvent)event;
             ((ArrowHitEntityEvent) event).getTarget().addPhysicalDamage(damage);
         }
     }
 
     @Override
     public String getLoreEntry() {
-        return TextColors.RED + "" + damage + " к урону стрел при ношении";
+        return Messages.getMessage("arrow-damage-suffix")
+                .replace("%DAMAGE%", Formatters.round.format(damage));
     }
 
 }
