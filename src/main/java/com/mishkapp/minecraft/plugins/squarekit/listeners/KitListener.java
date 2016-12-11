7734987dc64b59353de4e2d62609d828b2a22189
mishkapp <mishkapp@gmail.com>
2016-12-11 12:50:28 +0700

    Kills from same IP now will not be counted, deaths will

--- src/main/java/com/mishkapp/minecraft/plugins/squarekit/listeners/KitListener.java
+++ src/main/java/com/mishkapp/minecraft/plugins/squarekit/listeners/KitListener.java
@@ -1,11 +1,14 @@
 package com.mishkapp.minecraft.plugins.squarekit.listeners;
 
+import com.mishkapp.minecraft.plugins.squarekit.Messages;
 import com.mishkapp.minecraft.plugins.squarekit.events.PlayerKilledByEntityEvent;
 import com.mishkapp.minecraft.plugins.squarekit.events.PlayerKilledByPlayerEvent;
 import com.mishkapp.minecraft.plugins.squarekit.events.PlayerKilledEvent;
 import com.mishkapp.minecraft.plugins.squarekit.player.KitPlayer;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.filter.type.Exclude;
+
+import static com.mishkapp.minecraft.plugins.squarekit.utils.Utils._text;
 
 /**
  * Created by mishkapp on 29.10.2016.
@@ -29,9 +32,12 @@ public class KitListener {
         KitPlayer killed = event.getPlayer();
         KitPlayer killer = event.getKiller();
         if(killer != killed){
-            killer.onKill(killed);
+            if(killed.getMcPlayer().getConnection().getAddress().getAddress().equals(killer.getMcPlayer().getConnection().getAddress().getAddress())){
+                killer.getMcPlayer().sendMessage(_text(Messages.get("error-kill-from-same-ip")));
+            } else {
+                killer.onKill(killed);
+            }
         }
         killed.onDeath();
     }
-
 }
